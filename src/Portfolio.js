e0e0c6cb9b099f4c484244e8c5f236a22bd9dbd2
Adam Moore <adamoore@gmail.com>
2021-05-06 16:27:01 -0400

    fixed typos, added copy, and fixed margins on p elements.

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -24,9 +24,9 @@ function Portfolio() {
         <h3>
           <p>
             I made the first version of my protfilio site during my second year
-            at Parsons. I was very taken with a movement within web desing
-            called digital brutalism. There&apos;s a fair amount of writing
-            about digital brutalism out there.&nbsp;
+            at Parsons. At the time I was very taken with a movement within web
+            design called digital brutalism. There&apos;s a fair amount of
+            writing about digital brutalism out there.&nbsp;
             <a
               className="underline"
               href="https://uxdesign.cc/why-you-should-care-about-digital-brutalism-fa0c82d60133"
@@ -38,7 +38,25 @@ function Portfolio() {
           </p>
           <p>
             Digital brutalism is something I&apos;m still very much interested
-            in, but I understand it&apos;s not to everyone&apos;s taste.
+            in, but I understand it&apos;s not to everyone&apos;s taste. After
+            soliciting a lot of feedback from a diverse range of sources I began
+            to see a theme in their critique. The critique boiled down to
+            &quot;aesthetically the site is too unconventional, and it
+            doesn&apos;t do your work the justice&quot;.
+          </p>
+          <p>
+            Of course, feedback like this isn&apos;t the most fun to receive,
+            but it is probably the most valuable. Afterall a portfolio should
+            make one&apos;s work look as strong as possible. If the design of
+            the portfolio is distracting users from the work itself then it
+            needs to be redesigned.
+          </p>
+          <p>
+            The fruits of my research, and days spent playing in figma have lead
+            to this, adamoore.net V2.0. I do hope you like it, and that this
+            portfolio opens a window into my creative process. If you have any
+            feedback I&apos;d be delighted to hear it, V3.0 is not far off on
+            the horizon. Please click next, take a look around.
           </p>
           <p>
             <a
